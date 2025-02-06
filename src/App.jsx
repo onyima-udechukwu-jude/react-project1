ec046e8516536c8c81b32cdbe628eb31fad82061
Oghale <agarioghale10@gmail.com>
2025-02-06 15:22:54 +0100

    created a function

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
-export default function App(){
-    return(<h3>jude</h3>)
-    }
-    
+export default function App() {
+  return <h3>jude</h3>;
+}
+
+function Blessing(){
+   return <h3>this is blessing</h3> 
+}
